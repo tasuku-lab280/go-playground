c8c496311d6f88ff75f541cc07243548b8a9c6d5
tasuku-lab280 <loweng280@gmail.com>
2023-05-13 16:52:57 +0900

    .vscode追加

--- pkg/infrastructure/db.go
+++ pkg/infrastructure/db.go
@@ -1,16 +1,17 @@
 package infrastructure
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"os"
 )
 
 func NewDB() *gorm.DB {
 	db, err := gorm.Open(mysql.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
-  if err != nil {
-    panic("failed to connect database")
-  }
+	if err != nil {
+		panic("failed to connect database")
+	}
 
 	return db
 }
